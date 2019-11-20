abef3691f71b33d20db058651989784a1450f701
Nikhil Bhatia <nbhatia823@ucla.edu>
2019-11-19 23:56:58 -0800

    MODELS - updated PRA model and removed old files

--- classes/pra.py
+++ classes/pra.py
@@ -1,10 +1,11 @@
-from peewee import TextField, CharField, BooleanField, DateField
-from peewee_setup import BaseModel, UnknownField
+from peewee import TextField, CharField, BooleanField, DateField, TimestampField
+from peewee_setup import BaseModel, db_helper
+from playhouse.postgres_ext import ArrayField, JSONField
 
 class Pra(BaseModel):
     comments = TextField(null=True)
     county = CharField(null=True)
-    currentcontact = UnknownField(null=True)  # USER-DEFINED
+    currentcontact = JSONField(null=True)        # three keys: "name", "method", "method_info" with corresonding values    
     currentstatus = CharField(null=True)
     dataactionable = BooleanField(null=True)
     dataanalyzed = BooleanField(null=True)
@@ -15,18 +16,18 @@ class Pra(BaseModel):
     dateofrequest = DateField(null=True)
     enddaterequested = DateField(null=True)
     enddatereturned = DateField(null=True)
-    initialcontactmethod = CharField(null=True)
-    initialcontactinfo = CharField(null=True)
-    initialcontactperson = CharField(null=True)
+    initialcontact = JSONField(null=True)       # three keys: "name", "method", "method_info" with corresonding values
     issheriffsdept = BooleanField(null=True)
     lea = CharField(null=True)
     leadmember = CharField(null=True)
     linktoprarequest = CharField(null=True)
     startdaterequested = DateField(null=True)
     startdatereturned = DateField(null=True)
-    updates = UnknownField(null=True)  # USER-DEFINED
-    variables = UnknownField(null=True)  # ARRAY
+    updates = JSONField(null=True)              # each entry is stored with key as timestamp and value as the note
+    variables = JSONField(null=True)            # each entry is stored with variable name as key, and then two subkeys: 
+                                                ### the subkeys are requested (bool as value) and received (bool as value)
     variableschecked = BooleanField(null=True)
     variablescomplete = BooleanField(null=True)
 
 
+db_helper.create_tables([Pra])
