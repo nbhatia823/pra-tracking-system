28528b0a8a1a1823059ec0a6cccaab6a8fd85e36
Nikhil Bhatia <nbhatia823@ucla.edu>
2019-12-04 16:22:44 -0800

    [SETUP] - Formatting changes and remove pycache files

--- db.py
+++ db.py
@@ -4,12 +4,13 @@ from definitions import Definitions
 from functools import reduce
 import operator
 
+
 class Db:
 
-# ive sent all my responses as models right now. is that how i should send them? or should i send them as objects? consider a function that converts from an object to json or to a final ready to deliver state
+    # ive sent all my responses as models right now. is that how i should send them? or should i send them as objects? consider a function that converts from an object to json or to a final ready to deliver state
 
-    ### CRUD methods
-    ### All methods are static because we are not creating an instantiation of Db
+    # CRUD methods
+    # All methods are static because we are not creating an instantiation of Db
 
     # READ methods
 
@@ -18,7 +19,8 @@ class Db:
     def get_pras(filters=None, limit=Definitions.DEFAULT_NUM_ROW_ENTRIES, fields=Definitions.DEFAULT_PRA_FIELDS):
         if filters:
             expr_filters = Db.convert_filter_dict_to_peewee_expr(filters)
-            pras = Pra.select(*fields).where(expr_filters).limit(limit).dicts().execute()
+            pras = Pra.select(
+                *fields).where(expr_filters).limit(limit).dicts().execute()
         else:
             pras = Pra.select(*fields).limit(limit).dicts().execute()
         pras = [pra for pra in pras]
@@ -29,7 +31,8 @@ class Db:
     @staticmethod
     def get_pra(id, fields=Definitions.DEFAULT_PRA_FIELDS):
         print("attempting to print ")
-        pras = Pra.select(**fields).where(Pra.id == id).limit(1).dicts().execute()
+        pras = Pra.select(**fields).where(Pra.id ==
+                                          id).limit(1).dicts().execute()
         if len(pras) == 1:
             return pras[0]
         else:
@@ -44,7 +47,6 @@ class Db:
             return Pra.insert(**field_mapping).execute()
         except:
             return -1
-        
 
     # UPDATE methods
 
@@ -61,22 +63,17 @@ class Db:
     def delete_pra(id):
         return Pra.delete().where(Pra.id == id).execute()
 
-
-
     ######################################
 
-
-
-    ### Helper methods
+    # Helper methods
 
     # converts filters given as dict to peewee logical anded expression
     @staticmethod
     def convert_filter_dict_to_peewee_expr(filters):
         expression_list = [getattr(Pra, field) == value
-            for field, value in filters.items()]
+                           for field, value in filters.items()]
         anded_expr = reduce(operator.and_, expression_list)
         return anded_expr
-
 
     @staticmethod
     def convert_array_fields_to_peewee_fields(fields):
